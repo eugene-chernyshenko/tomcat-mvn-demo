bed520357b5b25d9405acb1fc530368a14372681
Eugene Chernyshenko <chernyshenko.eugene@gmail.com>
2021-09-22 04:19:35 +0300

    test props merging

--- src/main/java/com/example/demo/HelloServlet.java
+++ src/main/java/com/example/demo/HelloServlet.java
@@ -11,13 +11,25 @@ public class HelloServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        Properties props = new Properties();
-        InputStream input = getClass().getClassLoader().getResourceAsStream("some.properties");
-        props.load(input);
-        if (input != null) {
-            input.close();
+        Properties mainProps = new Properties();
+        InputStream inputMain = getClass().getClassLoader().getResourceAsStream("main.properties");
+        mainProps.load(inputMain);
+        if (inputMain != null) {
+            inputMain.close();
         }
+
+        Properties overrideProps = new Properties();
+        InputStream inputOverride = getClass().getClassLoader().getResourceAsStream("override.properties");
+        overrideProps.load(inputOverride);
+        if (inputOverride != null) {
+            inputOverride.close();
+        }
+
+        Properties mergedProps = new Properties();
+        mergedProps.putAll(mainProps);
+        mergedProps.putAll(overrideProps);
+
         response.setStatus(200);
-        response.getWriter().write("OK! foo: " + props.getProperty("foo"));
+        response.getWriter().write("foo: " + mergedProps.getProperty("foo") + ", bar: " + mergedProps.getProperty("bar"));
     }
 }
